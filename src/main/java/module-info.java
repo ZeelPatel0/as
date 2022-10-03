0a6fc2d99f523f231f7040ecfa3c53486d7b064d
zeelpatel <zeelgpatel01@gmail.com>
2022-10-02 21:46:24 -0400

    First commit of Assignment 1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment1gc200483258 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment1gc200483258 to javafx.fxml;
+    exports com.example.assignment1gc200483258;
+}
